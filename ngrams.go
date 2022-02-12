fde3a93fce4d2ce010e85273b8ed4d1120d7a981
Konstantin Dobratulin <ksdobratulin@gmail.com>
2022-02-12 18:31:58 +0300

    update docs in ngrams.go

--- ngrams.go
+++ ngrams.go
@@ -1,5 +1,7 @@
 package ngrams
 
+// Ngrams returns N-grams for input string, e.g.:
+// Ngrams("Hello!", 2) -> [He el ll lo o!]
 func Ngrams(line string, n int) []string {
 	if n <= 0 {
 		return []string{}
@@ -18,14 +20,20 @@ func Ngrams(line string, n int) []string {
 	return ngrams
 }
 
+// Unigrams returns 1-grams for input string, e.g.:
+// Unigrams("Hello!") -> [H e l l o !]
 func Unigrams(line string) []string {
 	return Ngrams(line, 1)
 }
 
+// Bigrams returns 2-grams for input string, e.g.:
+// Bigrams("Hello!") -> [He el ll lo o!]
 func Bigrams(line string) []string {
 	return Ngrams(line, 2)
 }
 
+// Trigrams returns 3-grams for input string, e.g.:
+// Trigrams("Hello!") -> [Hel ell llo lo!]
 func Trigrams(line string) []string {
 	return Ngrams(line, 3)
 }
